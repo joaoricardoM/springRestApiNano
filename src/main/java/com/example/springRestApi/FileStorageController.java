22dc87044cecbf21b7afbb19044b732804165c6c
joaoricardoM <joao_pia_mds@hotmail.com>
2024-02-02 17:01:23 -0300

    refactor

--- src/main/java/com/example/springRestApi/FileStorageController.java
+++ src/main/java/com/example/springRestApi/FileStorageController.java
@@ -8,7 +8,6 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
@@ -16,9 +15,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/files")
@@ -26,35 +31,39 @@ public class FileStorageController {
     private final Path fileStorageLocation;
 
     public FileStorageController(FileStorageProperties fileStorageProperties) {
-        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir()).toAbsolutePath().normalize();
+        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir())
+                .toAbsolutePath().normalize();
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadFile(@RequestParam("file")MultipartFile file) {
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 
         try {
             Path targetLocation = fileStorageLocation.resolve(fileName);
             file.transferTo(targetLocation);
 
-            String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("api/files/download/").path(fileName).toUriString();
+            String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path("/api/files/download/")
+                    .path(fileName)
+                    .toUriString();
 
-            return ResponseEntity.ok("Upload complete! Download link"+ fileDownloadUri);
-
+            return ResponseEntity.ok("File uploaded successfully. Download link: " + fileDownloadUri);
         } catch (IOException ex) {
-            return ResponseEntity.badRequest().build();
+            ex.printStackTrace();
+            return ResponseEntity.badRequest().body("File upload failed.");
         }
     }
 
     @GetMapping("/download/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) throws IOException {
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName,
+                                                 HttpServletRequest request) throws IOException {
         Path filePath = fileStorageLocation.resolve(fileName).normalize();
-
         try {
             Resource resource = new UrlResource(filePath.toUri());
 
             String contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-            if(contentType == null) {
+            if (contentType == null) {
                 contentType = "application/octet-stream";
             }
 
@@ -62,8 +71,8 @@ public class FileStorageController {
                     .contentType(MediaType.parseMediaType(contentType))
                     .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                     .body(resource);
-        } catch (MalformedURLException ex){
-            return ResponseEntity.badRequest().build();
+        } catch (MalformedURLException ex) {
+            return ResponseEntity.badRequest().body(null);
         }
     }
 
