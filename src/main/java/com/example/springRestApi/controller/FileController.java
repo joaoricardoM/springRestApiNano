560858fd9ad363f7cdf6c7a83b35b10d2a6cec7f
joaoricardoM <joao_pia_mds@hotmail.com>
2024-02-06 16:43:44 -0300

    feat: first endpoint

--- src/main/java/com/example/springRestApi/controller/FileController.java
+++ src/main/java/com/example/springRestApi/controller/FileController.java
@@ -1,10 +1,18 @@
 package com.example.springRestApi.controller;
 
+import com.example.springRestApi.model.File;
 import com.example.springRestApi.services.FileService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/files")
@@ -14,6 +22,19 @@ public class FileController {
     private final FileService service;
 
     /**
-     * Criação dos endpoints
+     * Endpoint para upload de um único arquivo.
+     *
+     * @param file O arquivo a ser carregado.
+     * @return Uma ResponseEntity contendo a URI do arquivo carregado.
      */
+
+    @PostMapping
+    public ResponseEntity upload(@RequestParam(value = "file") MultipartFile file) {
+        File fileSaved = service.save(file, service.extractTags(file));
+        return ResponseEntity.created(buildFileURL(fileSaved)).build();
+    }
+
+    private URI buildFileURL(File file) {
+        return ServletUriComponentsBuilder.fromCurrentRequestUri().path("/" + file.getId()).build().toUri();
+    }
 }
